b69674598ace3a1c6dbc95fad7dfbd9428582655
atsmaster <qazplmtest1122@gmail.com>
2022-10-04 17:55:02 +0900

    commit

--- iherb/Crawl.py
+++ iherb/Crawl.py
@@ -1,11 +1,16 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
 
+from iherb.model.Item import Item
+from iherb.model.ItemPool import ItemPool
+
 
 class Crawl:
 
     BASE_URL = 'https://kr.iherb.com'
     CARE_INFO_URL = '/info/links/'
+
+    item_pool = ItemPool()
 
     def iherb_info(self):
         soup = bs(req.get(self.BASE_URL + self.CARE_INFO_URL).text, 'html.parser')
@@ -13,6 +18,8 @@ class Crawl:
         care_info = soup.findAll('li', 'sticky-header-menu-navigation-list-item-header')
 
         # care, items, item
+
+        # 고민별 url 작업
         for care in care_info:
             care_url = care.find('a').attrs['href']
             care_nm = care.find('a').string
@@ -32,6 +39,7 @@ class Crawl:
                 # page
                 self.get_items(items_url)
 
+    # 상품들이 존재하는 페이지 접근
     def get_items(self, items_url):
         items_soup = bs(req.get(items_url).text, 'html.parser')
         items = items_soup.findAll('a', 'absolute-link product-link')
@@ -40,8 +48,20 @@ class Crawl:
             item_url = item_url.attrs['href']
             self.get_item(item_url)
 
+    # 상품에 접근
     def get_item(self, item_url):
         items_soup = bs(req.get(item_url).text, 'html.parser')
+
+        images = items_soup.find('div', 'thumbnail - container')
+
+        item = Item()
+        item.id = '111'
+
+        self.item_pool.add(item)
+
+        bb=0
+
+
 
 
 
